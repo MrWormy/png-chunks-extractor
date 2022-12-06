684caa680a1da5f0ea0721a9a4075d0461695225
Thomas <thomas.laurenc@gmail.com>
2022-12-06 20:28:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Basic PNG chunks parser
+
+```
+import chunkParser from 'png-chunks-extractor';
+
+const {chunks, chunksByType} = await chunkParser('path/to/file'[, options]);
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+import pce from 'lib/png-chunks-extractor.js';
+
+/**
+ * Default export, library entry point
+ * @function exports
+ * @exports png-chunks-extractor
+ * @requires module:lib/png-chunks-extractor
+ */
+export default pce;
