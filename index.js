14f6c1afed9008972197a10ca2fe57b12a6a569f
Thomas <thomas.laurenc@gmail.com>
2022-12-07 00:34:20 +0100

    updating doc ~ functional

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/**
+ * @module png-chunks-extractor
+ * @requires module:lib/png-chunks-extractor
+ */
 import pce from 'lib/png-chunks-extractor.js';
 
 /**
  * Default export, library entry point
  * @function exports
- * @exports png-chunks-extractor
- * @requires module:lib/png-chunks-extractor
+ * @return {Chunk} A {@link module:lib/png-chunks-extractor~Chunk|Chunk}
  */
 export default pce;
